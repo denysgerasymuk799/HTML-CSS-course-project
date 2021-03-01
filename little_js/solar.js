23778716e69b664dabb694655d73cd7f35bdf8c4
Yevhen Pankevych <y.pankevych@ucu.edu.ua>
2021-03-01 16:20:57 +0200

    added solar page

---
+++ little_js/solar.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+    $("#mercury-area").mouseover(function() {
+        $("#mercury").show("medium");
+    });
+    $("#mercury-area").mouseout(function() {
+        $("#mercury").hide("medium");
+    });
+
+    $("#venus-area").mouseover(function() {
+        $("#venus").show("medium");
+    });
+    $("#venus-area").mouseout(function() {
+        $("#venus").hide("medium");
+    });
+
+    $("#earth-area").mouseover(function() {
+        $("#earth").show("medium");
+    });
+    $("#earth-area").mouseout(function() {
+        $("#earth").hide("medium");
+    });
+
+    $("#mars-area").mouseover(function() {
+        $("#mars").show("medium");
+    });
+    $("#mars-area").mouseout(function() {
+        $("#mars").hide("medium");
+    });
+
+    $("#jupiter-area").mouseover(function() {
+        $("#jupiter").show("medium");
+    });
+    $("#jupiter-area").mouseout(function() {
+        $("#jupiter").hide("medium");
+    });
+
+    $("#saturn-area").mouseover(function() {
+        $("#saturn").show("medium");
+    });
+    $("#saturn-area").mouseout(function() {
+        $("#saturn").hide("medium");
+    });
+
+    $("#uranus-area").mouseover(function() {
+        $("#uranus").show("medium");
+    });
+    $("#uranus-area").mouseout(function() {
+        $("#uranus").hide("medium");
+    });
+
+    $("#neptune-area").mouseover(function() {
+        $("#neptune").show("medium");
+    });
+    $("#neptune-area").mouseout(function() {
+        $("#neptune").hide("medium");
+    });
+})
